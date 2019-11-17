e346375d5f0d3e1745f36acd6ef4fe7ed5a902cf
Amendoza20 <20.angel.mendoza@gmail.com>
2019-11-17 10:48:49 -0500

    push

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,56 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    Set<Integer> set = new LinkedHashSet<>(Arrays.asList(array));
+    public IntegerDuplicateDeleter(Integer[] array){
+        super(array);
+        Set<Integer> set = this.set;
+    }
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Map<Integer,Integer> map = new LinkedHashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(Integer x : set){
+            map.put(x,0);
+        }
+        for(Integer j : array){
+            map.replace(j, map.get(j) + 1);
+        }
+        for(Integer k : set){
+            if(map.get(k) < maxNumberOfDuplications){
+                for(int i = 0; i < map.get(k);i++){
+                    list.add(k);
+                }
+            }
+        }
+
+        return list.toArray(new Integer[0]);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<Integer,Integer> map = new LinkedHashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(Integer x : set){
+            map.put(x,0);
+        }
+        for(Integer j : array){
+            map.replace(j, map.get(j) + 1);
+        }
+        for(Integer k : set){
+            if(!map.get(k).equals(exactNumberOfDuplications)){
+                for(int i = 0; i < map.get(k);i++){
+                    list.add(k);
+                }
+            }
+        }
+
+        return list.toArray(new Integer[0]);
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,51 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    private Set<String> set = new LinkedHashSet<>();
+
+    public StringDuplicateDeleter(String[] array){
+        super(array);
+        Set<String> set = this.set;
+    }
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        List<String> list = new ArrayList<>();
+        for(String x : set){
+            map.put(x,0);
+        }
+        for(String k : array) {
+            map.replace(k, map.get(k) + 1);
+            if (map.get(k) < maxNumberOfDuplications){
+                for(int i = 0; i < map.get(k); i++){
+                    list.add(k);
+                }
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        List<String> list = new ArrayList<>();
+        for(String x : set){
+            map.put(x,0);
+        }
+        for(String k : array) {
+            map.replace(k, map.get(k) + 1);
+            if (!(map.get(k) < exactNumberOfDuplications)){
+                for(int i = 0; i < map.get(k); i++){
+                    list.add(k);
+                }
+            }
+        }
+        return list.toArray(new String[0]);
+    }
 }
